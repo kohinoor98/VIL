c20edfd59252bab096e9a2ce46cd4c51e54206d2
kohinoor98 <kohinoorchatterjee1998@gmail.com>
2020-02-13 17:15:57 +0530

    Shades changed

--- lib/pages/Dashboard/dashboard.dart
+++ lib/pages/Dashboard/dashboard.dart
@@ -13,12 +13,20 @@ class _DashboardState extends State<Dashboard> {
   TextEditingController rechargeController = new TextEditingController();
 
   static final List<String> imgList = [
-    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+    // 'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    // 'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    // 'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    // 'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    // 'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    // 'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+    // 'https://www.mbaskool.com/2017_images/stories/mar-images/mktgmx-idea.jpg',
+
+    'https://akm-img-a-in.tosshub.com/indiatoday/images/story/201911/idea-vodafone-Social-770x433.jpeg?47hbsmaxuvIGkPVZMTfwsBqChEpNqfvW',
+    'https://akm-img-a-in.tosshub.com/indiatoday/images/story/201809/Vodafone_Idea.jpeg?qItV6l5p8yWbPhYNFXJFGW.O8Vafw5oU',
+    'https://3.bp.blogspot.com/-Q_PqBvxHAkk/WEF39daU_OI/AAAAAAAAHOs/nCH48KkPLgMyF0ugy-mIIDUzmOfgQ_NCwCLcB/s320/Vodafone%2BGhana%2Bvoice%2Bpromotions.png',
+    // 'https://companies.naukri.com/idea-jobs/wp-content/uploads/sites/241/2014/02/slide1.jpg',
+    'https://image.slidesharecdn.com/ideavodamergerfinal19jan18-180318153951/95/idea-voda-merger-final-1-638.jpg?cb=1521387655',
+    'https://www.menosfios.com/wp-content/uploads/2017/04/0180_OpenGraph_Homepage.png',
   ];
 
 //Auto playing carousel
@@ -119,7 +127,7 @@ class _DashboardState extends State<Dashboard> {
                               style: TextStyle(
                                 fontFamily: 'Quicksand',
                                 fontSize: 25.0,
-                                color: Colors.grey,
+                                color: Colors.black,
                                 fontWeight: FontWeight.normal,
                               ),
                             ),
@@ -129,7 +137,7 @@ class _DashboardState extends State<Dashboard> {
                             style: TextStyle(
                               fontFamily: 'Quicksand',
                               fontSize: 25.0,
-                              color: Colors.grey,
+                              color: Colors.black,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
@@ -141,7 +149,7 @@ class _DashboardState extends State<Dashboard> {
                                     builder: (context) => MyCardsPage()),
                               );
                             },
-                            color: Colors.grey,
+                            color: Colors.black,
                             icon: Icon(Icons.add),
                           ),
                         ],
@@ -341,7 +349,7 @@ class _DashboardState extends State<Dashboard> {
                   ),
                 ),
                 SizedBox(width: 15.0),
-                ButtonAnimation(Color(0xFFFF4B2B), Colors.red),
+                ButtonAnimation(Color(0xFFEF0201), Colors.red),
               ],
             ),
 
@@ -367,8 +375,8 @@ class CustomMadeClipper extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
             gradient: LinearGradient(colors: [
-          const Color(0xFFFF4B2B),
-          const Color(0xFFFF4B2B),
+          const Color(0xFFEF0201),
+          const Color(0xFFEF0201),
         ])),
       ),
       clipper: CustomBlueClipper(),
